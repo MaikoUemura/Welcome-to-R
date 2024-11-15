971880286a3949e95d8eed9577f155bec1ac601a
MaikoUemura <m.uemura619.2@gmail.com>
2024-11-15 13:32:56 +0900

    test2

--- test1.R
+++ test1.R
@@ -3,13 +3,4 @@ y <- 3*4
 z <- y/x
 
 x+y+z
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 x+y-z
-=======
-x+y-z
->>>>>>> 42d529aac327fbf815636dc93aa86dcce2d5046b
-=======
-x+y-z
->>>>>>> 9d10c55fd4d3ac3378e8e3f014f7cbedc7758c92
